cfc4c337586bec52e01bed3c65a6b0d967c55ab5
Yan-Daojiang <yandaojiang@whu.edu.cn>
2022-05-17 21:21:38 +0800

    :new: add function min() and max()

--- src/raft/log.go
+++ src/raft/log.go
@@ -59,3 +59,17 @@ func (l *Log) String() string {
 	}
 	return fmt.Sprint(strings.Join(nums, "|"))
 }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
