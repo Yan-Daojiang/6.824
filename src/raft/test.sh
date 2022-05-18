f8e46e8bb5e00abade79f0e7232bcf6f340e2e7a
Yan-Daojiang <yandaojiang@whu.edu.cn>
2022-05-18 22:56:28 +0800

    :white_check_mark: 持续添加测试

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,7 +1,9 @@
-# python dtest.py TestInitialElection2A \
-# -n 100 \
-# -r 
-
-python dtest.py TestInitialElection2A TestReElection2A TestManyElections2A TestBasicAgree2B TestRPCBytes2B \
--n 100 \
+#!bin/bash
+python dtest.py \
+TestInitialElection2A \
+TestReElection2A \
+TestManyElections2A \
+TestBasicAgree2B \
+TestRPCBytes2B \
+-n 10000 \
 -r 
