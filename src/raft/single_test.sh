8dbf38530282d1c0450a51101e93336b2b38b56a
Yan-Daojiang <yandaojiang@whu.edu.cn>
2022-05-19 10:17:55 +0800

    :white_check_mark: Test 2B

--- src/raft/single_test.sh
+++ src/raft/single_test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python dtest.py TestBasicAgree2B -n 2000 -p 3 -r 
+python dtest.py \
+TestBasicAgree2B \
+TestRPCBytes2B \
+TestFailAgree2B \
+TestFailNoAgree2B \
+TestConcurrentStarts2B \
+TestRejoin2B \
+TestBackup2B \
+TestCount2B \
+-n 2000 -p 3 -r 
