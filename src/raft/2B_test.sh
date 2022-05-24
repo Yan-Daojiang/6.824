6c2ef5e27333c82b6b82adf64a125c9b82b15114
Yan-Daojiang <yandaojiang@whu.edu.cn>
2022-05-24 11:01:20 +0800

    :white_check_mark: TEST 2B

---
+++ src/raft/2B_test.sh
@@ -0,0 +1,12 @@
+python dtest.py \
+TestBasicAgree2B \
+TestRPCBytes2B \
+TestFailAgree2B
+TestFailNoAgree2B \
+TestConcurrentStarts2B \
+TestRejoin2B \
+TestBackup2B \
+TestCount2B \
+-n 10000 \
+-p 1 \
+-r
