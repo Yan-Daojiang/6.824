b4f0890b08ca7189bfa8b49ea07769bdec05dc1c
Yan-Daojiang <yandaojiang@whu.edu.cn>
2022-05-24 11:03:21 +0800

    :white_check_mark: TEST 2C

---
+++ src/raft/2C_test.sh
@@ -0,0 +1,11 @@
+python dtest.py \
+TestPersist12C \
+TestPersist22C \
+TestFigure82C \
+TestUnreliableAgree2C \
+TestFigure8Unreliable2C \
+TestReliableChurn2C \
+TestUnreliableChurn2C \
+-n 10000 \
+-p 1 \
+-r
