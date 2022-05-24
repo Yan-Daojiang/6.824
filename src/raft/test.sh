647e5d165357e0e48237d525ea8a95b3c0060344
Yan-Daojiang <yandaojiang@whu.edu.cn>
2022-05-24 10:53:58 +0800

    :white_check_mark: TEST 2A 2B 2C

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,16 +1,22 @@
-#!bin/bash
 python dtest.py \
 TestInitialElection2A \
 TestReElection2A \
 TestManyElections2A \
 TestBasicAgree2B \
 TestRPCBytes2B \
-TestFailAgree2B \
+TestFailAgree2B
 TestFailNoAgree2B \
 TestConcurrentStarts2B \
-TestRejoin2B \
+TestRejoin2B \  
 TestBackup2B \
 TestCount2B \
--n 10000 \
--p 2 \
--r 
+TestPersist12C \
+TestPersist22C \
+TestFigure82C \
+TestUnreliableAgree2C \
+TestFigure8Unreliable2C \
+TestReliableChurn2C \
+TestUnreliableChurn2C \
+-n 3000 \
+-p 1 \
+-r
